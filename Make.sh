cd5487b154ecc9574f18dd1903a5afb09a5fc31f
paytoshj2 <44882335+paytoshj2@users.noreply.github.com>
2019-11-21 21:10:55 -0500

    Add files via upload
    Adding reduce functionality (kind of) it uses Aggregation

---
+++ Make.sh
@@ -0,0 +1,5 @@
+sudo pip install spark
+sudo pip install boto
+sudo pip install warc
+sudo pip install pyspark
+sudo pip install https://github.com/commoncrawl/gzipstream/archive/master.zip
--- parseServerSUCCESS.py
+++ parseServerSUCCESS.py
@@ -42,22 +42,80 @@ def getHeaders (id_, iterator):
                 data = json.loads(line.payload.read())
 
                 # Possibly attempt to yield each header value instead of a dictionary, map should be forming the key-value pairs
-                yield data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Server", ""), \
-                    data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-XSS-Protection", ""), \
-                    data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Content-Security-Policy", ""), \
-                    data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Frame-Options", ""), \
-                    data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Strict-Transport-Security", ""), \
-                    data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Content-Type-Options", ""), \
-                    data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Download-Options", ""), \
-                    data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Permitted-Cross-Domain-Policies", ""), \
-                    data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Public-Key-Pins", ""), \
-                    data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Content-Security-Policy", ""), \
-                    data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Expect-CT", ""), \
-                    data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Feature-Policy", ""), \
-                    data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Date", ""), \
-                    data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Referer", ""), \
-                    data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Referer-Policy", "")
+                # yield data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Server", ""), \
+                #     data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-XSS-Protection", ""), \
+                #     data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Content-Security-Policy", ""), \
+                #     data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Frame-Options", ""), \
+                #     data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Strict-Transport-Security", ""), \
+                #     data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Content-Type-Options", ""), \
+                #     data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Download-Options", ""), \
+                #     data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Permitted-Cross-Domain-Policies", ""), \
+                #     data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Public-Key-Pins", ""), \
+                #     data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Content-Security-Policy", ""), \
+                #     data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Expect-CT", ""), \
+                #     data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Feature-Policy", ""), \
+                #     data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Date", ""), \
+                #     data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Referer", ""), \
+                #     data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Referer-Policy", "")
 
+                retArray = [None] * 15
+                retArray[0] = data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Server", "")
+                if(data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-XSS-Protection", "") != ""):
+                    retArray[1] = 1
+                else:
+                    retArray[1] = 0
+                if(data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Content-Security-Policy", "") != ""):
+                    retArray[2] = 1
+                else:
+                    retArray[2] = 0
+                if(data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Frame-Options", "") != ""):
+                    retArray[3] = 1
+                else:
+                    retArray[3] = 0
+                if(data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Strict-Transport-Security", "")!= ""):
+                    retArray[4] = 1
+                else:
+                    retArray[4] = 0
+                if(data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Content-Type-Options", "")!= ""):
+                    retArray[5] = 1
+                else:
+                    retArray[5] = 0
+                if(data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Download-Options", "")!= ""):
+                    retArray[6] = 1
+                else:
+                    retArray[6] = 0
+                if(data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Permitted-Cross-Domain-Policies", "")!= ""):
+                    retArray[7] = 1
+                else:
+                    retArray[7] = 0
+                if(data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Public-Key-Pins", "")!= ""):
+                    retArray[8] = 1
+                else:
+                    retArray[8] = 0
+
+                if(data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("X-Content-Security-Policy", "")!= ""):
+                    retArray[9] = 1
+                else:
+                    retArray[9] = 0
+                if(data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Expect-CT", "")!= ""):
+                    retArray[10] = 1
+                else:
+                    retArray[10] = 0
+                if(data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Feature-Policy", "")!= ""):
+                    retArray[11] = 1
+                else:
+                    retArray[11] = 0
+                retArray[12] = data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Date", "")
+                if(data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Referer", "")!= ""):
+                    retArray[13] = 1
+                else:
+                    retArray[13] = 0
+                if(data["Envelope"]["Payload-Metadata"]["HTTP-Response-Metadata"]["Headers"].get("Referer-Policy", "")!= ""):
+                    retArray[14] = 1
+                else:
+                    retArray[14] = 0
+
+                yield retArray
 
             except ValueError:
                 continue
@@ -68,9 +126,15 @@ def getHeaders (id_, iterator):
 
 files = sc.textFile("testwat.paths")
 headers = files.mapPartitionsWithIndex(getHeaders) \
-    .map(lambda x: (x[0], x[1:14]))
+    .map(lambda x: (x[0], (x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11], x[12], x[13], x[14]))) \
+    #.reduceByKey(lambda x, y: x + y)
+
+sumcount = headers.aggregateByKey((0,0,0,0,0,0),\
+    (lambda x, y: (x[0]+y[0],x[1]+y[1],x[2]+y[2], x[3]+y[3],x[4]+y[4],x[5]+y[5])),\
+    (lambda rdd1, rdd2: (rdd1[0]+rdd2[0], rdd1[1]+rdd2[1], rdd1[2]+rdd2[2], rdd1[3]+rdd2[3],rdd1[4]+rdd2[4],rdd1[5]+rdd2[5])))
+
 
 print("DONE")
 
-for x in headers.collect():
-    print x
+for x in sumcount.collect():
+    print x                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  ~                         

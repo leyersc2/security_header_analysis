7cedff1fd8b9fb061be718dead937b26b3011b1d
Leyers <leyersc2@thur114-03.win.winthrop.edu>
2020-01-21 19:12:41 -0500

    Added test comment

--- parseServer.py
+++ parseServer.py
@@ -117,6 +117,8 @@ def getHeaders (id_, iterator):
             except UnboundLocalError:
                 continue
 
+#TEST COMMENT 
+
 files = sc.textFile("testwat.paths")
 headers = files.mapPartitionsWithIndex(getHeaders) \
     .map(lambda x: (x[0], (x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11], x[12], x[13], x[14]))) \

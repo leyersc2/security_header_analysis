40f3ad2f06bf7ab3f1753936b3f42528a92c9c0b
Leyers <leyersc2@thur114-06.win.winthrop.edu>
2020-01-28 19:16:52 -0500

    Updated startup script

--- Make.sh
+++ Make.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+# AWS EMR Setup script
+
+sudo yum install git
+git clone https://github.com/leyersc2/security_header_analysis.git
+echo "~GIT Repo Cloned installed~"
+
+
+sudo pip install wheel
+echo clear
 sudo pip install spark
 sudo pip install boto
 sudo pip install warc
 sudo pip install pyspark
+echo clear
 sudo pip install https://github.com/commoncrawl/gzipstream/archive/master.zip
+echo "~Packages installed~"

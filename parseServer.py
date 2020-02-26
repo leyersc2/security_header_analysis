85f1a7c465df50d6c57cf04e57d636bcdf23e5c9
Leyers <leyersc2@thur114-07.win.winthrop.edu>
2020-02-25 19:20:04 -0500

    updating documentation

--- parseServer.py
+++ parseServer.py
@@ -1,3 +1,66 @@
+    # /*------------------------------------------------- parseServer.py -----
+    #  |  program name: parseServer.py
+    #  |
+    #  |  Authors: Connor Leyers, Joshua Paytosh, Nolan Worthy
+    #  |
+    #  |  Purpose:  Aimed at analyzing a subset of the CommonCrawl dataset to
+    #  |      assess WWW security of approximately 190 million unique hosts
+    #  |      by looking at active presence of HTTPS secuirty Headers across
+    #  |      multiple months in a crawled year.
+    #  |
+    #  |      This is done by sampling the data via an AWS S3 Bucket and
+    #  |      utilizing an Elastic Map Reduce (EMR) cluster to partition
+    #  |      and optimize the large scale computation required for this
+    #  |      analysis.
+    #  |
+    #  |  Parameters:
+    #  |             ***IN = USED TO PASS DATA INTO THIS FUNCTION
+    #  |                OUT = USED TO PASS DATA OUT OF THIS FUNCTION
+    #  |                IN/OUT = USED FOR BOTH PURPOSES***
+    #  |
+    #  |      CommonCrawl WARC Files (IN) -- Collection of WARC files
+    #  |          containing raw HTML data from each crawled hosts
+
+    #  |      CommonCrawl WAT Files (IN) -- Contains JSON formatted metadata
+    #  |        containing components desired for analysis on unique hosts
+    #  |
+    #  |  Returns: TBD
+    #  *-------------------------------------------------------------------*/
+
+
+
+    # /*------------------------------------------------- Packages -----
+    #  |    Packages:
+    #  |    json : Allows for object manipulation into/out of JSON formatting.
+    #  |
+    #  |    boto :Boto is the Amazon Web Services (AWS) SDK for Python.
+    #  |     It enables Python developers to create, configure, and manage
+    #  |     AWS services, such as EC2 and S3.
+    #  |
+    #  |    boto.s3.key -> key : The Key object is used in boto to keep
+    #  |     track of data stored in S3.
+    #  |
+    #  |    boto.s3.connection -> S3Connection : Connect to local
+    #  |     Region’s endpoint. Returns a connection object pointing
+    #  |     to the endpoint associated with this region.
+    #  |
+    #  |    gzipstream -> GzipStreamFile : allows Python to process
+    #  |     multi-part gzip files from a streaming source.
+    #  |
+    #  |    warc : extension of the ARC file format, which adds more
+    #  |     freedom by adding more metadata to each record
+    #  |     and allowing named headers.
+    #  |
+    #  |    pyspark -> SparkContext : SparkContext is the entry point to any
+    #  |     spark functionality.
+    #  |
+    #  |    urlparse -> urlparse : provides functions for breaking URLs
+    #  |    down into their component parts.
+    #  |
+    #  |    hashlib : implements a common interface to many different
+    #  |     secure hash and message digest algorithms.
+    #  *-------------------------------------------------------------------*/
+
 import json
 import boto
 from boto.s3.key import Key
@@ -10,10 +73,8 @@ import hashlib
 
 sc = SparkContext.getOrCreate()
 
-
 # SHOULD PROBABLY BE A FILESTREAM
 # f = open("/home/josh/Desktop/THESIS/DATA/CC-MAIN-20160524002110-00000-ip-10-185-217-139.ec2.internal.warc.wat")
-
 
 X_XSS_Protection_FLAG = 0b100000000000
 Content_Security_Policy_FLAG = 0b010000000000
@@ -94,5 +155,3 @@ for x in headers.take(1):
     print(x)
 for x in headers.take(1):
     print(x)
-
-

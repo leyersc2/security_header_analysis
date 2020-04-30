a7f5fd6d0502b492a1ce590b99ad564de5f81024
worthyn2 <54647701+worthyn2@users.noreply.github.com>
2020-04-30 15:05:35 -0400

    Documentation and renaming file

--- sampling.py
+++ sampling.py
@@ -1,3 +1,15 @@
+    # /*-------------------------------- sampling.py ----------------------
+    #  |    program name: sampling.py
+    #  |
+    #  |    Authors: Connor Leyers, Joshua Paytosh, Nolan Worthy
+    #  |
+    #  |    Purpose:  Takes a random sample of a specified number of file
+    #  |        paths for a given month.
+    #  |
+    #  |    Result: New paths file containing only the sampled paths with
+    #  |        the naming convention YY-MMwat.paths
+    #  *-------------------------------------------------------------------*/
+
 from random import randint
 from random import seed
 from time import time
@@ -8,12 +20,12 @@ seed(time())
 
 filesToSample = 40
 
-
+# loop by year, and by month within each year
 for yy in range(15, 21):
     for mm in range(1, 13):
         yymmstr = str(yy) + "-" + str(mm).zfill(2)
 
-        # tests for 2 possible filepaths. if neither are present, skip that month.
+        # tests for 2 possible filepath conventions. if neither are present, skip that month.
         if(path.exists("../paths/extracted/" + yymmstr + "wat.paths/" + yymmstr + "wat.paths")):
             inputFile = open("../paths/extracted/" + yymmstr + "wat.paths/" + yymmstr + "wat.paths", "r")
             pathType = 1
@@ -24,9 +36,9 @@ for yy in range(15, 21):
             continue
 
         lineCount = len(inputFile.readlines())
-        randList = []
+        randList = [] # list of random numbers generated so far for the current iteration
 
-        outputFile = open("secondSample/" + yymmstr + "wat.paths", "w")
+        outputFile = open("sampledPaths/" + yymmstr + "wat.paths", "w")
         for i in range(0, filesToSample):
             #generate random number
             randInt = randint(1,lineCount)
@@ -41,10 +53,11 @@ for yy in range(15, 21):
             elif(pathType == 2):
                 line = getline("../paths/extracted/" + yymmstr + "wat.paths/wat.paths", randInt)
 
+            ################ THIS CODE CHECKS IF THE NEW SAMPLE CONTAINS ANY PATHS FROM A PREVIOUS SAMPLE ################
+                
             # check previous random sample for current path. if it is present, generate new paths until it isn't.
-            with open("sampledPaths/" + yymmstr + "wat.paths", "r") as oldSample:
+            with open("prevSampleDir/" + yymmstr + "wat.paths", "r") as oldSample:
                 while line in oldSample.read():
-                    # sound the alarm
                     print("collision: " + line)
 
                     # generate new random number and get the corresponding path
@@ -56,6 +69,8 @@ for yy in range(15, 21):
 
                     # go back to beginning of previous sample file
                     oldSample.seek(0)
+                    
+            ##############################################################################################################
 
             randList.append(randInt)
             outputFile.write(line)
